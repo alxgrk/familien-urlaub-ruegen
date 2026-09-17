3c48b2dd6cf2b950948103e4499208296ccf0449
Alexander Girke-Duchstein <alexgirke@posteo.de>
2026-09-17 12:01:56 +0200

    chore: update next, typescript + postcss

--- next.config.js
+++ next.config.js
@@ -8,13 +8,7 @@
       ignoreBuildErrors: true,
     },
     transpilePackages: ['vis-timeline'],
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.node/,
-        use: "raw-loader",
-      });
-      return config;
-    },
+    turbopack: {},
   }
 
   module.exports = nextConfig
